fe3620686f35e8aee3a91db171925aa5296fdbc4
said <alsaghir.said@gmail.com>
2018-10-31 17:20:08 +0100

    adding delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: this.props.value
   };
   // instead for using constructor we can use arrow func to define this
   // constructor() {
@@ -13,9 +13,13 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
+  handleDelete = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
@@ -23,12 +27,18 @@ class Counter extends Component {
         >
           Increment
         </button>
-      </React.Fragment>
+        <button
+          onClick={this.handleDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 badge-";
+    let classes = "badge m-3 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,10 +4,12 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 5 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 }
+      { id: 4, value: 0 },
+      { id: 5, value: 0 },
+      { id: 6, value: 0 }
     ]
   };
 
